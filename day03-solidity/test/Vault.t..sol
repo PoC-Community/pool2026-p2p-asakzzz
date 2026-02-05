b5d2a080b4e6916678873b00c689f8da336af6b7
Leo Girard <leo.girard@epitech.eu>
2026-02-05 11:55:59 +0100

    Test day03 converts functional

--- day03-solidity/test/Vault.t..sol
+++ day03-solidity/test/Vault.t..sol
@@ -4,17 +4,23 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {PoolToken} from "../src/PoolToken.sol";
 
 
 contract VaultTest is Test {
     VaultHelper vault;
+    PoolToken token;
+
+    // address alice = makeAddr("alice");
+    uint256 constant INITIAL_SUPPLY = 1000 ether;
 
     function setUp() public {
-        vault = new VaultHelper(address(0x123));
+        token = new PoolToken(INITIAL_SUPPLY);
+        vault = new VaultHelper(address(token));
     }
 
     function test_Deployment() public view {
-        assertEq(address(vault.asset()), address(0x123));
+        assertEq(address(vault.asset()), address(token));
     }
 
     function test_InitialConversion() public view {
